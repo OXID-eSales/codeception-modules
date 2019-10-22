bfbc179f778a585647a8fd95298c0c82d02239d7
Florian Engelhardt <florian.engelhardt@oxid-esales.com>
2019-10-22 15:50:31 +0200

    OXDEV-2641 - allow for atomic activation of modules without dependencies

---
+++ src/Module/OxidModules.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Copyright © OXID eSales AG. All rights reserved.
+ * See LICENSE file for license details.
+ */
+
+namespace OxidEsales\Codeception\Module;
+
+require_once __DIR__.'/../../../../oxid-esales/testing-library/base.php';
+
+use OxidEsales\EshopCommunity\Internal\Framework\Module\Setup\Exception\ModuleSetupException;
+
+/**
+ * Class Oxideshop
+ * @package OxidEsales\Codeception\Module
+ */
+class OxidModules extends \Codeception\Module
+{
+    /**
+     * Reset context and activate modules before test
+     */
+    public function _beforeSuite($settings = [])
+    {
+        $this->activateModules();
+    }
+
+    /**
+     * Activates modules
+     */
+    private function activateModules()
+    {
+        $testConfig = new \OxidEsales\TestingLibrary\TestConfig();
+        $modulesToActivate = $testConfig->getModulesToActivate();
+
+        if ($modulesToActivate) {
+            $serviceCaller = new \OxidEsales\TestingLibrary\ServiceCaller();
+            $serviceCaller->setParameter('modulestoactivate', $modulesToActivate);
+            try {
+                $serviceCaller->callService('ModuleInstaller', 1);
+            } catch (ModuleSetupException $e) {
+                // this may happen if the module is already active,
+                // we can ignore this
+            }
+        }
+    }
+}
