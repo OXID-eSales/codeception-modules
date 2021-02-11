c85f918128cd44b77d12ff063148c1d1bb4b9107
vilmal <vilma.liorensaityte@gmail.com>
2021-02-11 17:06:28 +0100

    Setup shop without testing library

--- src/Module/Database/DatabaseTrait.php
+++ src/Module/Database/DatabaseTrait.php
@@ -22,16 +22,20 @@ trait DatabaseTrait
 
     public function setupShopDatabase()
     {
-        $facts = new Facts();
-        exec(
-            $facts->getCommunityEditionRootPath() .
-            '/bin/oe-console oe:database:reset' .
-            ' --db-host=' . $facts->getDatabaseHost() .
-            ' --db-port=' . $facts->getDatabasePort() .
-            ' --db-name=' . $facts->getDatabaseName() .
-            ' --db-user=' . $facts->getDatabaseUserName() .
-            ' --db-password=' . $facts->getDatabasePassword() .
-            ' --force'
+        $facts = new \OxidEsales\Facts\Facts();
+        $resetDatabaseService = new \OxidEsales\DeveloperTools\Framework\Database\Service\ResetDatabaseService(
+            new \OxidEsales\DeveloperTools\Framework\Database\Service\DatabaseChecker(),
+            new \OxidEsales\DeveloperTools\Framework\Database\Service\DatabaseCreator(),
+            new \OxidEsales\DeveloperTools\Framework\Database\Service\DatabaseInitiator(new \OxidEsales\EshopCommunity\Internal\Transition\Utility\BasicContext()),
+            new \OxidEsales\DeveloperTools\Framework\Database\Service\DropDatabaseService(),
+            new \OxidEsales\DatabaseViewsGenerator\ViewsGenerator()
+        );
+        $resetDatabaseService->resetDatabase(
+            $facts->getDatabaseHost(),
+            $facts->getDatabasePort(),
+            $facts->getDatabaseUserName(),
+            $facts->getDatabasePassword(),
+            $facts->getDatabaseName()
         );
     }
 
